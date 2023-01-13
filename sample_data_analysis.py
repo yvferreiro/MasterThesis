2229ca1485b936036dfb6b7d86304f3bee0d5599
rainbowjoy <dadu21ab@student.cbs.dk>
2023-01-13 14:24:46 +0100

    Generate a score
    All this currently does is make a summary table that shows the difference in proportion of gendered words. it's basic and not super helpful but it works!

--- sample_data_analysis.py
+++ sample_data_analysis.py
@@ -10,22 +10,56 @@ from nltk.tokenize import RegexpTokenizer
 
 df = pd.read_json ('sample_dataset.json', orient='split')
 n = len(pd.unique(df['source_id']))
-print(n)
 
 #I used this website to help with this section https://www.kirenz.com/post/2021-12-11-text-mining-and-sentiment-analysis-with-nltk-and-pandas-in-python/text-mining-and-sentiment-analysis-with-nltk-and-pandas-in-python/
 
 #this makes the article text all lower case
 df['full_description'] = df['full_description'].astype(str).str.lower()
-print(df[['full_description']].to_string(index=False))
+#print(df[['full_description']].to_string(index=False))
 
 #tokenization
 regexp = RegexpTokenizer('\w+')
 df['text_token']=df['full_description'].apply(regexp.tokenize)
-print(df[['text_token']].to_string(index=False))
 
 #stopwords but I'm not currently using this. I'm going to use words that might be on this list.
 #nltk.download('stopwords')
 #from nltk.corpus import stopwords
 #stopwords = nltk.corpus.stopwords.words("english")
 #df['text_token'] = df['text_token'].apply(lambda x: [item for item in x if item not in stopwords])
+
+df["Word Count"] = df["text_token"].str.len()
+
+his_w = ["his", "he", "man", "uncle", "dad", "father", "boy", "husband"]
+
+from collections import Counter
+
+df["His Count"] = (
+    df['full_description'].str.split()
+    .apply(Counter)
+    .apply(lambda counts: sum(word in counts for word in his_w))
+)
+
+her_w = ["her", "she", "woman", "aunt", "mother","mom", "girl", "wife"]
+
+
+from collections import Counter
+
+df["Her Count"] = (
+    df['full_description'].str.split()
+    .apply(Counter)
+    .apply(lambda counts: sum(word in counts for word in her_w))
+)
+
+
+Summarydf = df[['source_id','Her Count','His Count', 'Word Count']]
+Summarydf = Summarydf.groupby(['source_id']).sum()
+
+Summarydf = Summarydf[~(Summarydf['Word Count'] <= 10)]  
+
+Summarydf['Word Difference'] = Summarydf['Her Count']- Summarydf['His Count']
+Summarydf['Gendered Proportion'] = (Summarydf['Word Difference']/ Summarydf['Word Count'])*100
+
+Summary = Summarydf.drop(columns = ['His Count', 'Her Count', 'Word Difference','Word Count'])
+Summary = Summary.sort_values(by=['Gendered Proportion'])
+print(Summary)
 
