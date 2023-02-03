66e1893f4696f80de42faf08fe8c3fafa040a3c7
yferreirofranchi@gmail.com <100618395+yvferreiro@users.noreply.github.com>
2023-02-03 17:24:21 +0100

    I included some code to identify split a df into first and last names and subsequently identify the gender of the first name

--- name and gender detection.py
+++ name and gender detection.py
@@ -19,4 +19,24 @@ for nltk_result in nltk_results:
 #above code comes from an online article. I'm thinking that I could use a pandas 
 #df to figure out how to detect a name and mark the sentence Y or N or whatever. 
 
-#need to clearly use more research. lol
+#YF add 
+#import the Genderize package which we need to recognize names & pandas
+#pip install genderize
+from genderize import Genderize
+import pandas as pd
+
+#function to detect the gender 
+def genderize(name): 
+    return Genderize().get([name])[0]["gender"]
+
+#dummy df with sample names 
+names = ["Shirley Temple", "Clark Gables", "Georgle Jungle", "George Clooney", "Danielle Duncan", "Yolanda Ferreiro"]
+df_names = pd.DataFrame(names, columns = ["names"])
+
+#extract first name from a df of first and last names & create column only with first names
+df_names.loc[df_names["names"].str.split().str.len() == 2, "first name"] = df_names["names"].str.split().str[0]
+df_names
+
+# apply the genderize function to all of the rows in the df_names dataset with regards to the "first name" column
+df_names["gender"]= df_names.apply(lambda row : genderize(row["first name"]),axis=1)
+print(df_names)
