6d886a35826dc2adf3eb4af3e5e8b12f84a63c77
rainbowjoy <dadu21ab@student.cbs.dk>
2023-02-03 15:30:19 +0100

    Update name and gender detection.py
    First draft

--- name and gender detection.py
+++ name and gender detection.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  3 15:08:21 2023
+import nltk
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
 
-@author: danie
-"""
+sample = "John was a nice guy and had a friend named Earl"
+text = '''
+This is a sample text that contains the name Alex Smith who is one of the developers of this project.
+You can also find the surname Jones here.
+'''
 
+nltk_results = ne_chunk(pos_tag(word_tokenize(sample)))
+for nltk_result in nltk_results:
+    if type(nltk_result) == Tree:
+        name = ''
+        for nltk_result_leaf in nltk_result.leaves():
+            name += nltk_result_leaf[0] + ' '
+        print ('Type: ', nltk_result.label(), 'Name: ', name)
+        
+#above code comes from an online article. I'm thinking that I could use a pandas 
+#df to figure out how to detect a name and mark the sentence Y or N or whatever. 
+
+#need to clearly use more research. lol
