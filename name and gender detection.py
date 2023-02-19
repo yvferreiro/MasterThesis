1e6e33e884cd0bbb295b1fb8fd8082f5f1463560
yferreirofranchi@gmail.com <100618395+yvferreiro@users.noreply.github.com>
2023-02-19 21:59:16 +0100

    Added some comments on the Genderize function (around non-western names and the ability to check first and last name, combined). Added some new functions to categorize the outputs of the name_function function and created a new DB for gender (that also identifies when no gender can be attributed - i.e like in the case of news publications where no author is identified and it just says publication name). Cleaned up the outputs and made it all work together cohesively.
    Issues that still need to be addressed are in the notes at the end of
    the code. I moved "older" code that no longer serves a purpose to the
    bottom and called it "OLDER CODE".

--- name and gender detection.py
+++ name and gender detection.py
@@ -51,31 +51,30 @@ named_individuals = name_function(article_df, article_df['creator'])
 names3_cat = []
 names3_name = []
 
+#This function prints the categories of name of person (based on 1st name), 1st name if person identified, and then creates a list with 1-hot encoding 
+#for the outputs of "name_individuals", a variable created to capture the output of function "named_function". 
+#Important note: I plug in first and last names because if we only plug in first names, it identifies people as GPE when it is not the case. 
+
 def extract_cat(lst):
-    for i in range(len(lst)): #list of list of list 
-        for j in range(len(lst[i])): #list of list (list of first name + labels and last name + labels)
-            if len(lst[j]) == 0: #if len = 0 then it means the name_function didn't think it was a name 
-                names3_cat.append("cannot categorize")
-                names3_name.append("none ")
-            else:  
-                #for g in range(len(lst[j])): #if len = 2 then there is a label and a name
-                if len(lst[j]) == 2: #if len = 2 then there is a label and a name
-                    names3_cat.append([item[0] for item in lst[j]])
-                    names3_name.append([item[1] for item in lst[j]])
-                    #return [item[0] for item in lst[i]]
-                if len(lst[j]) == 1: #if len = 1 then there is a label but no name associated to it 
-                    names3_cat.append("no label identified")
-                    names3_name.append("no person identified ")
-                        #return ("no author identified")
+    for j in range(len(lst)): #list of list (list of first name + labels and last name + labels)
+        if len(lst[j]) == 0: 
+            names3_cat.append("cannot categorize person or organization")
+            names3_name.append("None ")
+        elif len(lst[j]) == 2: #if len > 1 then there is a label and a name
+            names3_cat.append([item[0] for item in lst[j]])
+            names3_name.append([item[1] for item in lst[j]])
+        else: 
+            names3_cat.append("no label identified")
+            names3_name.append("no person identified")
+
     return names3_cat, names3_name
 
-
-names2 = extract_cat(named_individuals) #confirms almost all individuals are picked up as person
+#confirms almost all individuals are picked up as person
+names2 = extract_cat(named_individuals) 
 
 #Create a new DF storing 1st names and gender (independent)
 names2_list_cat = list((names2)[0])
 names2_list_name = list((names2)[1])
-print(names2_list_cat)
 
 #function to grab category of 1st name 
 first_cat = []
@@ -89,10 +88,33 @@ def cats1(categories):
 
 cat1 = cats1(names2_list_cat) #name category variable "cat1"
 
+#function to only grab first from a list with first and last name 
+def extracto_first_name(column):
+    """
+    This function takes in a pandas column and outputs the first value of a list
+    if the item passed for a row is a list and output the value of the item if it is not.
+    """
+    return column.apply(lambda x: x[0] if isinstance(x, list) else x)
 
-df_1 = pd.DataFrame(list_together, columns = ["Label", "Names"])
-df_1.insert(2, "Category", cat1, True)
-df_1
+names2_list_name = extracto_first_name(df_1["Names"])
+
+#create DF with labels, names, and categories
+df_1 = pd.DataFrame(np.column_stack([cat1, names2_list_name]), columns = ["Label", "Names"], dtype = object)
+
+
+#apply genderize function to the author first name column in article_df
+def genderize(name): 
+    if name == "None": #None means there is no name available 
+        return "not identifiable"
+    elif name == "CBS" or name == "ABC": #manually weed out news publications 
+        return "not identifiable"
+    else: 
+        return Genderize().get([name])[0]["gender"]
+article_df['creator gender']= article_df.apply(lambda row : genderize(row["author_first_name"]),axis=1)
+article_df[['creator', "author_first_name", 'creator gender']]
+
+## Note: because it picks out media publications as "males", I added some lines of code to manually weed out 
+## publication names that have been occurring. 
 
 #function to get the gender of the first name 
 def genderize(name): 
@@ -101,17 +123,24 @@ article_df['creator gender']= article_df.apply(lambda row : genderize(row["autho
 print(article_df['creator gender'])
 print(article_df['creator'])
 
+## COMMENTS ON GENDERIZE: 
+# (a) I read the documentation and it seems that Genderize is fully capable of identifying non-Western names. 
+# As part of its parameters, you can input the ISO country or language code to tailor the search of names in that place
+# or in that language. It is cool, but I am not sure if we want to specify that given how international the range of folks reporting
+# in English is. 
+# (b)Genderize does not have the ability to check for the gender of a combine name. It scans through each part of the item and attributes gender. 
+#(c) The limit of names per API request is 10, so that is in line with the news data.  
 
 
 #EDA get count of male v. female creators 
 sns.set_theme(style="whitegrid")
-article_df['creator gender'].value_counts().plot(kind='bar', color = ["tomato", "skyblue"])
+article_df['creator gender'].value_counts().plot(kind='bar', color = ["tomato", "skyblue", "grey"])
 
-#Notes
-# The iterator seems to pick non-Western names & attribute the proper gender - i.e. "Sandali" is rightfully categorized as female. 
+#Notes 
 # The functions get confused when there are multiple authors, we need to figure out how to deal with this 
 # New DF with name and associated gender has been created --> "df_1"
 # New function was added in order to facilitate the process of attributing personhood to a name - useful to show the name_function works 
+
 
 
 
@@ -184,9 +213,12 @@ for nltk_result in nltk_results:
 from genderize import Genderize
 import pandas as pd
 
-#function to detect the gender 
+#function to get the gender of the first name 
 def genderize(name): 
     return Genderize().get([name])[0]["gender"]
+article_df['creator gender']= article_df.apply(lambda row : genderize(row["author_first_name"]),axis=1)
+print(article_df['creator gender'])
+print(article_df['creator'])
 
 #dummy df with sample names 
 names = ["Shirley Temple", "Clark Gables","ashley Smith", "Alex G", "Priantha G", "John G", "john G", "jon G", "Blake Shelton", "Blake Lively", "Georgle Jungle", "George Clooney", "Danielle Duncan", "Yolanda Ferreiro"]
