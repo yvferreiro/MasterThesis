586f231060a40dbbe22ba28437b96959722e21e3
rainbowjoy <dadu21ab@student.cbs.dk>
2023-03-09 15:38:49 +0100

    almost there

--- Logistic Regression V0.py
+++ Logistic Regression V0.py
@@ -1,6 +1,26 @@
-import pickle 
 import pandas as pd
+import nltk
+nltk.download('punkt')
 
-filename = "articles.pkl"
+filename = 'articles.pkl'
 
 article_df = pd.read_pickle(filename)
+article_df = article_df.assign(Article_Number=range(len(article_df)))
+article_df = article_df.reset_index()
+article_df.info()
+
+def split_sentences(article, article_id):
+    sentences = nltk.sent_tokenize(article)
+    sentences_with_id = [(sentence, article_id) for sentence in sentences]
+    return sentences_with_id
+
+sentences_list = []
+
+
+for article, article_id in article_df[['content','Article_Number']].values:
+    sentences = split_sentences(article, article_id)
+    sentences_list.extend(sentences)
+
+sentences_df = pd.DataFrame(sentences_list, columns= ['sentences', 'article_id'])
+
+sentences_df
