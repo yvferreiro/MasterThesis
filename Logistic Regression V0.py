0f6b2dc1bc1659bf9ea82e3364722efb77f85e34
yferreirofranchi@gmail.com <100618395+yvferreiro@users.noreply.github.com>
2023-03-09 15:47:41 +0100

    Fixed the issue with the strings & added comments

--- Logistic Regression V0.py
+++ Logistic Regression V0.py
@@ -9,6 +9,7 @@ article_df = article_df.assign(Article_Number=range(len(article_df)))
 article_df = article_df.reset_index()
 article_df.info()
 
+# tokenize sentences in an article 
 def split_sentences(article, article_id):
     sentences = nltk.sent_tokenize(article)
     sentences_with_id = [(sentence, article_id) for sentence in sentences]
@@ -16,11 +17,13 @@ def split_sentences(article, article_id):
 
 sentences_list = []
 
-
+# add sentences to a new DF along with article ID 
 for article, article_id in article_df[['content','Article_Number']].values:
-    sentences = split_sentences(article, article_id)
+    sentences = split_sentences(str(article), article_id)
     sentences_list.extend(sentences)
 
 sentences_df = pd.DataFrame(sentences_list, columns= ['sentences', 'article_id'])
 
 sentences_df
+
+# TO-DO: add the data & find the pronouns to determine gender
